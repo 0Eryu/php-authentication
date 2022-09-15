93d614af1b5723f54ce1e32b79191613e2f85784
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2022-09-15 10:16:11 +0200

    Ajout de l'attribut et du constructeur à la classe UserProfile

--- src/Html/UserProfile.php
+++ src/Html/UserProfile.php
@@ -2,8 +2,25 @@
 declare(strict_types=1);
 
 namespace Html;
+use Entity\User;
 
 class UserProfile
 {
+    use StringEscaper;
+
+    /**
+     * @var User Utilisateur.
+     */
+    private User $user;
+
+    /**
+     * @param User $user
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+
 
 }
