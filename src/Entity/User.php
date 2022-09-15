ed3db835c3dc8e20ada31cc450c748f761e9f7ca
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2022-09-15 09:29:48 +0200

    Ajout des attributs de la classe User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,5 +5,25 @@ namespace Entity;
 
 class User
 {
+    /**
+     * @var int Identifiant de l'utilisateur
+     */
+    private int $id;
+    /**
+     * @var string Prénom de l'utilisateur
+     */
+    private string $firstname;
+    /**
+     * @var string Nom de famille de l'utilisateur
+     */
+    private string $lastname;
+    /**
+     * @var string Mot de passe de l'utilisateur
+     */
+    private string $login;
+    /**
+     * @var string Numéro de téléphone de l'utilisateur
+     */
+    private string $phone;
 
 }
