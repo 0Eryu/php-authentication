8846c28a7f0c163a0f942b559fe05459effe7275
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2022-09-15 11:05:12 +0200

    Ajout de la méthode loginForm dans la classe UserAuthentication

--- src/Authentication/UserAuthentication.php
+++ src/Authentication/UserAuthentication.php
@@ -4,8 +4,32 @@ declare(strict_types=1);
 
 namespace Authentication;
 
+use Html\StringEscaper;
+
 class UserAuthentication
 {
+    use StringEscaper;
+
     private const LOGIN_INPUT_NAME = 'login';
     private const PASSWORD_INPUT_NAME = 'password';
+
+    /**
+     * @param string $action
+     * @param string $submitText
+     * @return string Formulaire de connexion.
+     */
+    public function loginForm(string $action, string $submitText = 'OK') : string
+    {
+        $login = self::LOGIN_INPUT_NAME;
+        $password = self::PASSWORD_INPUT_NAME;
+
+        return <<<HTML
+            <form action={$action} method="POST">
+                <input type="text" name={$login} id={$login} placeholder={$login}>
+                <input type="password" name={$password} id={$password} placeholder={$password}>
+                <input type="submit" value={$submitText}>
+            </form>
+        HTML;
+
+    }
 }
