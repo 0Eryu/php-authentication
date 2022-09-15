7fc39245772c5efdfa1c9b5ce1078914829c8f6a
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2022-09-15 10:07:57 +0200

    Ajout de la méthode de classe findByCredentials de la classe User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,10 @@ declare(strict_types=1);
 
 namespace Entity;
 
+use Database\MyPdo;
+use Entity\Exception\EntityNotFoundException;
+use PDO;
+
 class User
 {
     /**
@@ -64,6 +68,35 @@ class User
     public function getPhone(): string
     {
         return $this->phone;
+    }
+
+    /**
+     * Recherche dans la base de donnée l'utilisateur correspondant au login et au mot de passe passés
+     * en paramètres.
+     *
+     * @param string $login Identifiant de l'utilisateur.
+     * @param string $password Mot de passe de l'utilisateur.
+     * @return User Utilisateur correspondant à l'identifiant et au mot de passe mis en paramètres.
+     */
+    public static function findByCredentials(string $login, string $password) : User
+    {
+        $query = MyPdo::getInstance()->prepare(
+            <<<SQL
+            SELECT id, lastName, firstName, login, phone
+            FROM user
+            WHERE login = :login AND sha512pass = SHA2(:password, 512)
+            SQL
+        );
+
+        $query->execute([':login' => $login, ':password' => $password]);
+        $query->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, User::class);
+        $user = $query->fetch();
+
+        if ($user === false) {
+            throw new EntityNotFoundException('Aucun utilisateur correspondant à cet identifiant et à ce mot de passe a été trouvé dans la base de donnée');
+        }
+
+        return $user;
     }
 
 }
