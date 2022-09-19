3a6876619084528cde5349e16fa5bfcb4c0f6d26
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2022-09-19 14:33:49 +0200

    Ajout de la méthode findById dans la classe UserAvatar

--- src/Entity/UserAvatar.php
+++ src/Entity/UserAvatar.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Entity;
 
+use Database\MyPdo;
+use Entity\Exception\EntityNotFoundException;
+use PDO;
+
 class UserAvatar
 {
     /**
@@ -30,5 +34,31 @@ class UserAvatar
     public function getPng(): ?string
     {
         return $this->png;
+    }
+
+    /**
+     * @param int $userId Identifiant de l'utilisateur dans la base de donnée.
+     * @return $this Chaine de caractère correspondant à un avatar au format PNG.
+     * @throws EntityNotFoundException Utilisateur non trouvé avec l'identifiant placé en paramètre.
+     */
+    public function findById(int $userId): self
+    {
+        $query = MyPdo::getInstance()->prepare(
+            <<<SQL
+                SELECT id, avatar
+                FROM user
+                WHERE id = :userId
+            SQL
+        );
+
+        $query->execute([':userId' => $userId]);
+        $query->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, UserAvatar::class);
+        $user = $query->fetch();
+
+        if ($user === false) {
+            throw new EntityNotFoundException('Aucun utilisateur correspondant à cet identifiant et à ce mot de passe a été trouvé dans la base de donnée');
+        }
+
+        return $user;
     }
 }
