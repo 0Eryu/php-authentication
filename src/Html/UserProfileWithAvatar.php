37a34c9389b6f25090bd81671be891bd0bc12989
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2022-09-22 10:36:37 +0200

    Ajout du constructeur de la classe UserProfileWithAvatar.php

--- src/Html/UserProfileWithAvatar.php
+++ src/Html/UserProfileWithAvatar.php
@@ -4,13 +4,24 @@ declare(strict_types=1);
 
 namespace Html;
 
+use Entity\User;
+
 class UserProfileWithAvatar extends UserProfile
 {
+
     public const AVATAR_INPUT_NAME = 'avatar';
     /**
      * @var string
      */
     private string $formAction;
+
+    /**
+     * @param string $formAction
+     */
+    public function __construct(string $formAction)
+    {
+        $this->formAction = $formAction;
+    }
 
     public function toHtml(): string
     {
