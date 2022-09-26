42285d0bd4f1ee4120fac23c58e4932ebda41dad
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2022-09-26 14:32:48 +0200

    Ajout de la méthode getInBytes dans la classe Directive.php

--- src/ServerConfiguration/Directive.php
+++ src/ServerConfiguration/Directive.php
@@ -13,4 +13,18 @@ class Directive
     {
         return ini_get($directive);
     }
+
+    public function getInBytes(string $directive) : int
+    {
+        $directiveValue = $this->get($directive);
+        preg_match('/([0-9]+)([gmk]?)/i', $directiveValue, $matches, PREG_OFFSET_CAPTURE);
+        switch ($matches[2]) {
+            case 'k':
+                $matches[1] * 1024;
+            case 'm':
+                $matches[1] * 1024;
+            case 'g':
+                $matches[1] * 1024;
+        }
+    }
 }
